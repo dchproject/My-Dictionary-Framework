e68182c9627e142abc426c2c1068166f3d5dcfbc
Chumakov Dmytro <dmytro.v.chumakov.work@gmail.com>
2022-08-05 09:25:03 +0300

    main added: - MyDictionaryFramework.xcodeproj

---
+++ MyDictionaryFramework/MyDictionaryFramework/MyDictionaryFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyDictionaryFramework.h
+//  MyDictionaryFramework
+//
+//  Created by Dmytro Chumakov on 05.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyDictionaryFramework.
+FOUNDATION_EXPORT double MyDictionaryFrameworkVersionNumber;
+
+//! Project version string for MyDictionaryFramework.
+FOUNDATION_EXPORT const unsigned char MyDictionaryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyDictionaryFramework/PublicHeader.h>
+
+
